f8b27243804a4aef862997d3eb4c2df6a21e1cd7
soberdorfer <sebastian.oberdorfer@topicus.nl>
2025-07-01 23:12:17 +0200

    Simple array of Refs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,75 @@
-use std::collections::HashSet;
+use std::any::type_name;
 
-struct Voting {
-    first: Vec<i16>
-    second: Vec<i16>
-    third: Vec<i16>
+enum Vote {
+    In,
+    Out,
+    Place(u8),
+}
+
+struct Name<'a> {
+    group: u8,
+    round: u8,
+    name_ref: &'a str,
+    vote: Vote,
 }
 
 fn main() {
-    let names = vec![
-        "Everett", "Elio", "Fergus", "Gilbert", "Jarvis", "Jed", "Magnus", "Otis", "Parker",
+    let names: Vec<&'static str> = vec![
+        "Provide", "Names", "Here", "And", "As", "Many", "As", "You", "Want",
     ];
-    let groups: Vec<Vec<&str>> = create_balanced_groups(names);
-    let votes: HashSet<i16, Voting> = vote_groups(groups)
-    verify_votes(votes)
+
+    let ref_names: Vec<Name> = names
+        .iter()
+        .map(|name| Name {
+            group: 0,
+            round: 0,
+            name_ref: name,
+            vote: Vote::In,
+        })
+        .collect();
+
+    for round in u8::from(1)..reduce_t(names.len()) {
+        let mut group_num = 3;
+        let filtered: Vec<(usize, &mut Name)> = ref_names
+            .iter_mut()
+            .enumerate()
+            .filter_map(|(index, name)| match name.vote {
+                Vote::Out => None,
+                _ => {
+                    name.round = round;
+                    name.group = &group_num;
+                }
+            })
+            .collect();
+        groups.insert(round, create_balanced_groups(names));
+        let votes: HashSet<u8, Vote> = vote_groups(groups);
+        names = process_votes(votes, &names)
+    }
 }
 
-fn vote_groups(groups: Vec<Vec<&str>>) -> HashSet<i16, Voting> {
+fn process_votes(votes: HashSet<u8, Vote>, names: &Vec<&str>) -> Vec<&str> {
+    votes
+        .iter()
+        .map(|key, value| names.iter().find(|name| name == value.third));
     todo!()
 }
 
-fn create_balanced_groups(names: Vec<&str>) -> Vec<Vec<&str>> {
-    let group_size = 3
+fn vote_groups(groups: Vec<Vec<&str>>) -> HashSet<u8, Vote> {
+    todo!()
+}
+
+fn create_balanced_groups(names: Vec<&str>) -> Vec<&Vote> {
+    let group_size = 3;
     let amount = names.len();
     let unbalanced: bool = amount % group_size > 0;
     todo!()
+}
+
+fn reduce_t<T: TryFrom<usize>>(rx: usize) -> T {
+    match rx.try_into() {
+        Ok(n) => n,
+        Err(_) => {
+            panic!("Out of bounds for target {}!", type_name::<T>());
+        }
+    }
 }
